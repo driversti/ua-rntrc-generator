57ee4d6066d033f8a176e087435c6a15f572bde1
driversti <driversti@gmail.com>
2021-07-20 17:27:11 +0200

    Improved SexGenerator (and renamed from SexRandomizer)

--- src/main/java/com/github/driversti/rntrc/RntrcGenerator.java
+++ src/main/java/com/github/driversti/rntrc/RntrcGenerator.java
@@ -4,29 +4,29 @@ public class RntrcGenerator {
 
   private final BirthdayDaysGenerator birthdayDaysGenerator;
   private final PersonSerialNumberGenerator serialNumberGenerator;
-  private final SexRandomizer sexRandomizer;
+  private final SexGenerator sexGenerator;
   private final ControlDigitCalculator controlDigitCalculator;
 
   public RntrcGenerator() {
     this.birthdayDaysGenerator = new BirthdayDaysGenerator();
     this.serialNumberGenerator = new PersonSerialNumberGenerator();
-    this.sexRandomizer = new SexRandomizer();
+    this.sexGenerator = new SexGenerator();
     this.controlDigitCalculator = new ControlDigitCalculator();
   }
 
   public RntrcGenerator(BirthdayDaysGenerator birthdayDaysGenerator,
-      PersonSerialNumberGenerator serialNumberGenerator, SexRandomizer sexRandomizer,
+      PersonSerialNumberGenerator serialNumberGenerator, SexGenerator sexGenerator,
       ControlDigitCalculator controlDigitCalculator) {
     this.birthdayDaysGenerator = birthdayDaysGenerator;
     this.serialNumberGenerator = serialNumberGenerator;
-    this.sexRandomizer = sexRandomizer;
+    this.sexGenerator = sexGenerator;
     this.controlDigitCalculator = controlDigitCalculator;
   }
 
   public String rntrc() {
     final String days = birthdayDaysGenerator.days();
     final String serialNumber = serialNumberGenerator.serialNumber();
-    final String sex = sexRandomizer.sex();
+    final String sex = sexGenerator.sex();
     final String first9Digits = days + serialNumber + sex;
     final String controlDigit = controlDigitCalculator.controlDigit(first9Digits);
     return first9Digits + controlDigit;
--- src/main/java/com/github/driversti/rntrc/SexGenerator.java
+++ src/main/java/com/github/driversti/rntrc/SexGenerator.java
@@ -2,21 +2,21 @@ package com.github.driversti.rntrc;
 
 import java.util.Random;
 
-public class SexRandomizer {
+public class SexGenerator {
 
   private final Random random;
 
-  public SexRandomizer() {
+  public SexGenerator() {
     this.random = new Random();
   }
 
-  public SexRandomizer(Random random) {
+  public SexGenerator(Random random) {
     this.random = random;
   }
 
   String sex() {
     final int minInclusive = 0;
-    final int maxExclusive = 2;
+    final int maxExclusive = 9;
     return String.valueOf(random.nextInt(maxExclusive - minInclusive) + minInclusive);
   }
 }
